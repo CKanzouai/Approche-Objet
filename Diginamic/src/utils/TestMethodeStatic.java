69ea5231d5c9e4740270aef31484e4e847c52961
chaimae <chaimae.kanzouai@usmba.ac.ma>
2023-11-03 10:18:41 +0100

    nv commit

--- Diginamic/src/utils/TestMethodeStatic.java
+++ Diginamic/src/utils/TestMethodeStatic.java
@@ -1,4 +1,15 @@
 package utils;
+import java.lang.Integer;
 
 public class TestMethodeStatic {
+    public static void main(String[] args) {
+        String chaine = "12";
+        int nvValeur = Integer.parseInt(chaine);
+        System.out.println(nvValeur);
+        int a = 11;
+        int b = 56;
+        int max = Integer.max(a, b);
+        System.out.println(max);
+
+    }
 }
